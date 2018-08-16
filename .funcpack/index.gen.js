574bdab0e1994865467d18d9dc0d0633ffaf3d42
BriswellCompany <son.ph@briswell.com>
2018-08-16 11:35:59 +0900

    TTTS-443【子課題】POSデータ集約

--- .funcpack/index.gen.js
+++ .funcpack/index.gen.js
@@ -1,4 +1,5 @@
 module.exports = {
     "merge_function": require("../merge_function/index.js"),
+    "update_every_day_function": require("../update_every_day_function/index.js"),
     "update_function": require("../update_function/index.js")
 }
